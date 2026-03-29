8bc32b0b8691977cd193a253aec7c4c91c962497
TigRig <tigrig.crex@gmail.com>
2026-03-29 16:41:57 +0900

    save: 2026-03-29 16:41:57

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is {x}");
-    x = 6;
-    println!("The value of x is {x}");
+    let x: u8 = 255;
+    let x1 = x.wrapping_add(1);
+    let x2 = x.checked_add(1);
+    let (x3, b1) = x.overflowing_add(1);
+    let x4 = x.saturating_add(1);
+    println!("wrapping_add 255 + 1 = {x1}");
+    println!("checked_add 255 + 1 = {:?}", x2);
+    println!("overflowing_add 255 + 1 = {x3}, {b1}");
+    println!("saturating_add 255 + 1 = {x4}");
 }
